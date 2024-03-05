7dd6e3759334f7048781f8a8b212a54bb4004a51
Sebastian Agudelo <agudelojoan28@gmail.com>
2024-03-05 16:24:03 -0500

    Inicio de proyecto Sebastian

---
+++ index.js
@@ -0,0 +1,5 @@
+const { app } = require("./src/app");
+
+app.listen(3001, () => {
+    console.log('%s listening at 3001'); 
+}); 
---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require('cors')
+
+const app = express();
+
+// const accesList = ['http://localhost:5173', 'http://localhost:5174' ] app.use(cors({ origin: accesList })); uso de cors limitado
+
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+app.use(cors());
+
+module.exports = {
+    app,
+  };
