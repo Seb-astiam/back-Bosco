8a3736f362c45bf40f604bd684e8cf365f69013b
GIrigoin <guilleirigoin@hotmail.com>
2024-03-12 01:01:22 -0300

    Modelo User

---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  return sequelize.define("Videogame", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
