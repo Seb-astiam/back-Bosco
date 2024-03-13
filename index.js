5a47fd65284707f6d33b4cf0d8ab039140b3595b
GIrigoin <guilleirigoin@hotmail.com>
2024-03-13 11:55:30 -0300

    Db conected-Guille

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const PORT = 3001;
-const { conn } = require("./DB_connection");
+const { conn } = require("./DB_conection");
 const { app } = require("./src/app");
 
 conn.sync({ alter: true }).then(() => {
   app.listen(PORT, async () => {
-    await updateDBGenres();
     console.log("Server raised in port: " + PORT);
   });
 });
