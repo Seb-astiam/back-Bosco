0c94e3960c9aa651e836c91e080102dc9ac89b47
GIrigoin <guilleirigoin@hotmail.com>
2024-03-13 00:06:31 -0300

    Cambios Modelo User-Guille

---
+++ src/DB_conection.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_NAME}`,
+  { logging: false, native: false }
+);
