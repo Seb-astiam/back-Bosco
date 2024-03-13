be1bede06d36b6f0ab954379edaac76e2564ef30
GIrigoin <guilleirigoin@hotmail.com>
2024-03-13 00:21:47 -0300

    DB_connection- inicializar modelos y relaciones- Guille

--- src/DB_conection.js
+++ src/DB_conection.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const HousingModel = require("./Models/Housing");
+const MascotaModel = require("./Models/Mascota");
+const UserModel = require("./Models/User");
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_NAME}`,
   { logging: false, native: false }
 );
+
+HousingModel(sequelize);
+MascotaModel(sequelize);
+UserModel(sequelize);
+
+const { Housing, Mascota, User } = sequelize.models;
+
+module.exports = { conn: sequelize, Housing, Mascota, User };
