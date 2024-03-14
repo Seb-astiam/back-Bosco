444f99fdd2c3fcac5838a2725046710b06960f03
GIrigoin <guilleirigoin@hotmail.com>
2024-03-14 13:49:54 -0300

    ruta location completa -Guille

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const PORT = 3001;
 const { conn } = require("./src/DB_conection");
 const { app } = require("./src/app");
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   app.listen(PORT, async () => {
     console.log("Server raised in port: " + PORT);
   });
