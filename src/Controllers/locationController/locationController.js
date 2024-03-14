ab761cd2a53fb7dc4d049a49ddca065f65b2e399
GIrigoin <guilleirigoin@hotmail.com>
2024-03-14 11:08:39 -0300

    controller getProvinces - Guille

---
+++ src/Controllers/locationController/locationController.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+const URL = "https://apis.datos.gob.ar/georef/api/";
+
+const getProvinces = async () => {
+  try {
+    const { data } = await axios(`${URL}provincias?campos=id,nombre`);
+    const { provincias } = data;
+    return provincias;
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
+
+module.exports = getProvinces;
