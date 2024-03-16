fb79d72bdc34e57f9e78886cc1ce1a80f021b9df
GIrigoin <guilleirigoin@hotmail.com>
2024-03-15 23:36:28 -0300

    login -guille

---
+++ src/Controllers/loginController/loginController.js
@@ -0,0 +1,15 @@
+const { User } = require("../../DB_conection");
+
+const loginController = async (email, password) => {
+  try {
+    const user = await User.findOne({ where: { email } });
+    if (!user) throw Error("No hay usuario registrado con el mail ingresado");
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    return isPasswordValid;
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
+
+module.exports = { loginController };
