3599e1d98944f312916cdd4bf0b88f89ff52a794
GIrigoin <guilleirigoin@hotmail.com>
2024-03-15 21:09:47 -0300

    ruta profile completa- Guille

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const routerUser = require("./UserRoute");
 const { routerMascota } = require("./MascotaRoute");
 const routerLocation = require("./locationRoute");
+const routerProfile = require("./profileRoute");
 const router = Router();
 
 router.use("/user", routerUser);
@@ -9,5 +10,7 @@ router.use("/user", routerUser);
 router.use("/", routerMascota);
 
 router.use("/location", routerLocation);
+
+router.use("/profile", routerProfile);
 
 module.exports = router;
