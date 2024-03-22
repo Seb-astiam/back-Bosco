83047c25220effa40fc92936b997a9181f1b8ae4
Sebastian Agudelo <agudelojoan28@gmail.com>
2024-03-22 08:26:46 -0500

    ok

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const { conn } = require("./src/DB_conection");
 const { app } = require("./src/app");
-const { PORT } = process.env;
+// const { PORT } = process.env;
+const port = process.env.PORT || 3001
 
 conn.sync({ force: false }).then(() => {
-  app.listen(PORT || 3000, () => {
-    console.log(`On work port: ${PORT}`)
+  app.listen(port, () => {
+    console.log(`On work port: ${port}`)
 })
 });
