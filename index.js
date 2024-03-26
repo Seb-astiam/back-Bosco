8256f72470daa25a603c021c5be590f5f29d48a4
Sebastian Agudelo <agudelojoan28@gmail.com>
2024-03-25 19:57:27 -0500

    resolvi conflicto index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/DB_conection");
 const { app } = require("./src/app");
 
 conn.sync({ force: false }).then(() => {
-  app.listen(process.env.PORT || 3000, () => {
-    console.log("On work")
+  app.listen(process.env.PORT || 3001, () => {
+    console.log("On work", process.env.PORT)
 })
 });
