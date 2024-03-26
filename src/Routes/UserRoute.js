736abe935b6ba1d8b9575acb006a6599a8895e89
Sebastian Agudelo <agudelojoan28@gmail.com>
2024-03-25 21:17:52 -0500

    listo

--- src/Routes/UserRoute.js
+++ src/Routes/UserRoute.js
@@ -12,11 +12,7 @@ const routerUser = Router();
 routerUser.post("/", postUser);
 routerUser.get("/", getUsers);
 routerUser.get("/:email", getUserEmail);
-<<<<<<< HEAD
 routerUser.delete("/:email", delUser);
-=======
-routerUser.delete("/:email", delUser);
->>>>>>> 9ebe548 (handler controller googlelogin guille)
 routerUser.put("/", updateUserProfile);
 
 module.exports = routerUser;
