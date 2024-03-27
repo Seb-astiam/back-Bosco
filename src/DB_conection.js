45142d6b82243f471ba86cf7c0de8e757b79bdb4
GIrigoin <guilleirigoin@hotmail.com>
2024-03-27 10:07:36 -0300

    DB conection

--- src/DB_conection.js
+++ src/DB_conection.js
@@ -38,6 +38,9 @@ Service.belongsToMany(Housing, { through: "ServicexHousing" });
 User.hasOne(Profile);
 Profile.belongsTo(User);
 
+User.belongsToMany(Role, { through: "UserRole" });
+Role.belongsToMany(User, { through: "UserRole" });
+
 module.exports = {
   conn: sequelize,
   Housing,
