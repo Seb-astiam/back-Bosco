af87ea47a220a05bfffba7ad25946844bc54cfde
Sebastian Agudelo <agudelojoan28@gmail.com>
2024-04-14 15:29:10 -0500

    cambiando pagina para activate cuenta

--- src/Utils/createActivationBody.js
+++ src/Utils/createActivationBody.js
@@ -3,7 +3,7 @@ const createActivationBody = (token, name) => {
       <h1>Hola ${name}</h1>
       <h2>¡Bienvenido a Bosco!</h2>
       <p>Activa tu cuenta de Bosco accediendo al enlace de abajo</p>
-      <a href="http://localhost:5174/activate-account?token=${token}"><h3>Activar cuenta</h3></a>
+      <a href="https://front-bosco.up.railway.app/activate-account?token=${token}"><h3>Activar cuenta</h3></a>
       `;
 
   return body;
